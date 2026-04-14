7239d37e912cfebc99d9f4bea210bdbfebb8f9e3
als-wl <minnji852@naver.com>
2026-04-14 11:26:44 +0900

    메인 헤더 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './assets/css/main.css'
+import "./assets/css/main.css";
+import Header from "./layout/public/Header";
 
 function App() {
-
   return (
     <>
-
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
