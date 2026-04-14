97e857e6b70641617a24ebf23ef916e8da4b9247
S.Yunseo <sonyunsea@gmail.com>
2026-04-14 16:39:18 +0900

    [FEATURE] 관리자 페이지 레이아웃 제작 #2

---
+++ src/pages/admin/PaymentList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PaymentList() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PaymentList
---
+++ src/pages/admin/TrainerManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TrainerManagement() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TrainerManagement
