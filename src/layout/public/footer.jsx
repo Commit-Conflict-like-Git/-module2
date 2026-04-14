7a545626d0dd3f1a5e9733deb51b278d67308a62
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-04-14 10:37:33 +0900

    [FEATURE] 공통 CSS 파일 생성

---
+++ src/layout/public/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+function footer() {
+    return (
+        <footer>
+            <div>(주) 오늘의 훈련</div>
+            <div>Copyright 2026, ALL right reserved</div>
+        </footer>
+    )
+}
+
+export default footer
