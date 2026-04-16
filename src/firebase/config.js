d33deb328f569862fcb89041156277863b88c197
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-04-16 13:23:11 +0900

    [FEATURE]회원가입-파이어베이스 연동

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'     //db 객체 얻기 위한 함수
+import { getAuth } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +20,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 //firestore 라이브러리 사용을 위한 객체 가져오기
+export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 console.log(db);
